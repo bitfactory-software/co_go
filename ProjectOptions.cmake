3e7f4ee58533f4d23e257033d56c4319849c60c6
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-11-01 19:05:34 +0100

    rename CMake project to cogoproject

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -7,7 +7,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(cogoproject_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND NOT WIN32)
 
     message(STATUS "Sanity checking UndefinedBehaviorSanitizer, it should be supported on this platform")
@@ -54,183 +54,183 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(cogoproject_setup_options)
+  option(cogoproject_ENABLE_HARDENING "Enable hardening" ON)
+  option(cogoproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    cogoproject_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    cogoproject_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  cogoproject_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR cogoproject_PACKAGING_MAINTAINER_MODE)
+    option(cogoproject_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(cogoproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(cogoproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(cogoproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(cogoproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(cogoproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(cogoproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(cogoproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(cogoproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(cogoproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(cogoproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(cogoproject_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(cogoproject_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(cogoproject_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(cogoproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(cogoproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(cogoproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(cogoproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(cogoproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(cogoproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(cogoproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(cogoproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(cogoproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(cogoproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(cogoproject_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(cogoproject_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_USER_LINKER
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      cogoproject_ENABLE_IPO
+      cogoproject_WARNINGS_AS_ERRORS
+      cogoproject_ENABLE_USER_LINKER
+      cogoproject_ENABLE_SANITIZER_ADDRESS
+      cogoproject_ENABLE_SANITIZER_LEAK
+      cogoproject_ENABLE_SANITIZER_UNDEFINED
+      cogoproject_ENABLE_SANITIZER_THREAD
+      cogoproject_ENABLE_SANITIZER_MEMORY
+      cogoproject_ENABLE_UNITY_BUILD
+      cogoproject_ENABLE_CLANG_TIDY
+      cogoproject_ENABLE_CPPCHECK
+      cogoproject_ENABLE_COVERAGE
+      cogoproject_ENABLE_PCH
+      cogoproject_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  cogoproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (cogoproject_ENABLE_SANITIZER_ADDRESS OR cogoproject_ENABLE_SANITIZER_THREAD OR cogoproject_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(cogoproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(cogoproject_global_options)
+  if(cogoproject_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    cogoproject_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  cogoproject_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(cogoproject_ENABLE_HARDENING AND cogoproject_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR cogoproject_ENABLE_SANITIZER_UNDEFINED
+       OR cogoproject_ENABLE_SANITIZER_ADDRESS
+       OR cogoproject_ENABLE_SANITIZER_THREAD
+       OR cogoproject_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${cogoproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${cogoproject_ENABLE_SANITIZER_UNDEFINED}")
+    cogoproject_enable_hardening(cogoproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(cogoproject_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(cogoproject_warnings INTERFACE)
+  add_library(cogoproject_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  cogoproject_set_project_warnings(
+    cogoproject_warnings
+    ${cogoproject_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
     "")
 
-  if(myproject_ENABLE_USER_LINKER)
+  if(cogoproject_ENABLE_USER_LINKER)
     include(cmake/Linker.cmake)
-    myproject_configure_linker(myproject_options)
+    cogoproject_configure_linker(cogoproject_options)
   endif()
 
   include(cmake/Sanitizers.cmake)
-  myproject_enable_sanitizers(
-    myproject_options
-    ${myproject_ENABLE_SANITIZER_ADDRESS}
-    ${myproject_ENABLE_SANITIZER_LEAK}
-    ${myproject_ENABLE_SANITIZER_UNDEFINED}
-    ${myproject_ENABLE_SANITIZER_THREAD}
-    ${myproject_ENABLE_SANITIZER_MEMORY})
+  cogoproject_enable_sanitizers(
+    cogoproject_options
+    ${cogoproject_ENABLE_SANITIZER_ADDRESS}
+    ${cogoproject_ENABLE_SANITIZER_LEAK}
+    ${cogoproject_ENABLE_SANITIZER_UNDEFINED}
+    ${cogoproject_ENABLE_SANITIZER_THREAD}
+    ${cogoproject_ENABLE_SANITIZER_MEMORY})
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(cogoproject_options PROPERTIES UNITY_BUILD ${cogoproject_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(cogoproject_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      cogoproject_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(cogoproject_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    cogoproject_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(cogoproject_ENABLE_CLANG_TIDY)
+    cogoproject_enable_clang_tidy(cogoproject_options ${cogoproject_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(cogoproject_ENABLE_CPPCHECK)
+    cogoproject_enable_cppcheck(${cogoproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(cogoproject_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    cogoproject_enable_coverage(cogoproject_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(cogoproject_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(cogoproject_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(cogoproject_ENABLE_HARDENING AND NOT cogoproject_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR cogoproject_ENABLE_SANITIZER_UNDEFINED
+       OR cogoproject_ENABLE_SANITIZER_ADDRESS
+       OR cogoproject_ENABLE_SANITIZER_THREAD
+       OR cogoproject_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    cogoproject_enable_hardening(cogoproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
